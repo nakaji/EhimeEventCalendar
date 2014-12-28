b144f0f8fa76894fc3dec8b36f7b1af7a378059d
nakaji <nakaji126@gmail.com>
2014-12-28 16:50:01 +0900

    モデルを追加

---
+++ EhimeEventCalendar/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EhimeEventCalendar.Models
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<EventInfo> EventInfos { get; set; }
+    }
+}
---
+++ EhimeEventCalendar/Models/EventInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Remoting.Metadata;
+using System.Web;
+
+namespace EhimeEventCalendar.Models
+{
+    public class EventInfo
+    {
+        public int Id { get; set; }
+        
+        public string Title { get; set; }
+        
+        public string Description { get; set; }
+        
+        public DateTime StartTime { get; set; }
+        
+        public DateTime EndTime { get; set; }
+        
+        public string PlaceName { get; set; }
+        
+        public string PlaceAddress { get; set; }
+        
+        public string PlaceUrl { get; set; }
+
+        [Timestamp]
+        public byte[] TimeStamp { get; set; } 
+    }
+}
