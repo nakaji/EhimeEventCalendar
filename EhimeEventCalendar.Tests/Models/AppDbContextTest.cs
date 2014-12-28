66e233456c25a0c356103b7b8f49ded072027aa7
nakaji <nakaji126@gmail.com>
2014-12-28 16:50:15 +0900

    テストを追加

---
+++ EhimeEventCalendar.Tests/Models/AppDbContextTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using EhimeEventCalendar.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EhimeEventCalendar.Tests.Models
+{
+    [TestClass]
+    public class AppDbContextTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var db = new AppDbContext();
+            db.EventInfos.Add(new EventInfo()
+            {
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now,
+            });
+            db.SaveChanges();
+
+            Assert.AreEqual(1, db.EventInfos.Count());
+        }
+    }
+}
---
+++ EhimeEventCalendar.Tests/TestInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+using EhimeEventCalendar.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EhimeEventCalendar.Tests
+{
+    [TestClass]
+    public class TestInitializer
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", AppDomain.CurrentDomain.BaseDirectory);
+
+            Database.SetInitializer(new DropCreateDatabaseAlways<AppDbContext>());
+        }
+    }
+}
