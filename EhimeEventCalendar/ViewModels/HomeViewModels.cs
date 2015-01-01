009fd1bfea66f6e1707ebf923c03ac0a890b2e72
nakaji <nakaji126@gmail.com>
2015-01-02 01:44:25 +0900

    トップページ用のViewModelを追加

---
+++ EhimeEventCalendar/ViewModels/HomeViewModels.cs
@@ -0,0 +1,16 @@
+﻿using EhimeEventCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EhimeEventCalendar.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public List<EventInfo> EventInfos { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Days { get; set; }
+    }
+}
