ee7d6df503bb80e4663cd6a28cbb3c3b178015c2
nakaji <nakaji126@gmail.com>
2015-01-05 23:07:45 +0900

    自動マイグレーションを有効にする

--- EhimeEventCalendar/Global.asax.cs
+++ EhimeEventCalendar/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using EhimeEventCalendar.Models;
+using EhimeEventCalendar.Migrations;
 
 namespace EhimeEventCalendar
 {
@@ -16,6 +19,8 @@ namespace EhimeEventCalendar
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppDbContext, Configuration>());
         }
     }
 }
