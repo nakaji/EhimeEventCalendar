8385c82629c3e2d9bf27ec53f199ee5481c5987b
nakaji <nakaji126@gmail.com>
2015-01-20 02:17:55 +0900

    テストコードをリファクタリグ

--- EhimeEventCalendar.Tests/Exteisions/UtilTest.cs
+++ EhimeEventCalendar.Tests/Exteisions/UtilTest.cs
@@ -13,34 +13,31 @@ namespace EhimeEventCalendar.Tests.Exteisions
     [TestClass]
     public class UtilTest
     {
-        [TestMethod]
-        public void GetValue_指定した値が存在する場合はその値を返す()
+        private ModelBindingContext _bindingContext;
+
+        [TestInitialize]
+        public void SetUp()
         {
             var mock = new Mock<IValueProvider>();
             mock.Setup(c => c.GetValue("Venue.Name")).Returns(new ValueProviderResult("会場名", "会場名", null));
 
-            var bindingContext = new ModelBindingContext
+            _bindingContext = new ModelBindingContext
             {
                 ModelName = "Venue",
                 ValueProvider = mock.Object
             };
+        }
 
-            Assert.AreEqual("会場名", Util.GetValue(bindingContext, "Name"));
+        [TestMethod]
+        public void GetValue_指定した値が存在する場合はその値を返す()
+        {
+            Assert.AreEqual("会場名", Util.GetValue(_bindingContext, "Name"));
         }
 
         [TestMethod]
         public void GetValue_指定した値が存在する場合は空文字列を返す()
         {
-            var mock = new Mock<IValueProvider>();
-            mock.Setup(c => c.GetValue("Venue.Name")).Returns(new ValueProviderResult("会場名", "会場名", null));
-
-            var bindingContext = new ModelBindingContext
-            {
-                ModelName = "Venue",
-                ValueProvider = mock.Object
-            };
-
-            Assert.AreEqual("", Util.GetValue(bindingContext, "Address"));
+            Assert.AreEqual("", Util.GetValue(_bindingContext, "Address"));
         }
     }
 }
