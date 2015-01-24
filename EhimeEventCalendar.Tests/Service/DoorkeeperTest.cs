22d33dee5297103dc89ad2bc574ccd07532dca2c
nakaji <nakaji126@gmail.com>
2015-01-24 12:20:16 +0900

    DoorkeeperのAPIからイベント情報を取ってくる部分を実装

---
+++ EhimeEventCalendar.Tests/Service/DoorkeeperTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EhimeEventCalendar.Service;
+
+namespace EhimeEventCalendar.Tests.Service
+{
+    [TestClass]
+    public class DoorkeeperTest
+    {
+        [TestMethod]
+        public void GetById_イベントID指定でイベント情報を取得する()
+        {
+            var sut = new Doorkeeper();
+
+            var @event = sut.GetById(19916);
+
+            Assert.AreEqual("Agile459 #29 今年をアジャイルプランニング！", @event.Title);
+        }
+
+        [TestMethod]
+        public void GetById_該当するデータがない場合は空のクラスを返す()
+        {
+            var sut = new Doorkeeper();
+
+            var @event = sut.GetById(0);
+
+            Assert.AreEqual("", @event.Title);
+        }
+    }
+}
---
+++ EhimeEventCalendar/Service/Doorkeeper.cs
@@ -0,0 +1,42 @@
+﻿using EhimeEventCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Codeplex.Data;
+
+namespace EhimeEventCalendar.Service
+{
+    public class Doorkeeper
+    {
+        public EventInfo GetById(int id)
+        {
+            var url = "http://api.doorkeeper.jp/events/{0}";
+
+            var @event = new EventInfo();
+
+            using (var wc = new WebClient())
+            {
+                string res;
+                try
+                {
+                    res = wc.DownloadString(string.Format(url, id));
+                }
+                catch (WebException e)
+                {
+                    return new EventInfo();
+                }
+                var json = DynamicJson.Parse(res);
+                @event.Title = @json.@event.title;
+                @event.StartTime = DateTime.Parse( @json.@event.starts_at);
+                @event.EndTime =DateTime.Parse(@json.@event.ends_at);
+                @event.Contents = @json.@event.description;
+                @event.Url = @json.@event.public_url;
+                @event.Venue.Name = @json.@event.venue_name;
+            }
+
+            return @event;
+        }
+    }
+}
