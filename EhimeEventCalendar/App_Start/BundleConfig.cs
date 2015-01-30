a46f7d2866dc50ca75ac24ec8aa1fc766ec9a719
nakaji <nakaji126@gmail.com>
2015-01-31 04:29:25 +0900

    Bundleしてしまうとディレクトリが変わるのでDKEditorが動かなくなる

--- EhimeEventCalendar/App_Start/BundleConfig.cs
+++ EhimeEventCalendar/App_Start/BundleConfig.cs
@@ -27,7 +27,6 @@ namespace EhimeEventCalendar
                       "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/myscripts").Include(
-                     "~/Scripts/ckeditor/ckeditor.js",
                      "~/Scripts/MyScript.js"));
             
             bundles.Add(new StyleBundle("~/Content/css").Include(
