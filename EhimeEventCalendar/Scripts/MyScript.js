2f1da3c1ef7adc81723e76b4bbbd8280a24225a2
nakaji <nakaji126@gmail.com>
2015-02-04 01:34:51 +0900

    日付のセット方法が間違ってた

--- EhimeEventCalendar/Scripts/MyScript.js
+++ EhimeEventCalendar/Scripts/MyScript.js
@@ -10,11 +10,11 @@
             success: function (json) {
                 $("#Title").val(json.Title);
                 var date = new Date(json.StartTime);
-                $("#StartTime_Date").val(date.getFullYear() + "/" + ("0"+(date.getMonth() + 1)).slice(-2) + "/" + ("0"+date.getDay())).slice(-2);
+                $("#StartTime_Date").val(date.getFullYear() + "/" + ("0" + (date.getMonth() + 1)).slice(-2) + "/" + ("0" + date.getDate()).slice(-2));
                 $("#StartTime_Hour").val(date.getHours());
                 $("#StartTime_Minute").val(date.getMinutes());
                 date = new Date(json.EndTime);
-                $("#EndTime_Date").val(date.getFullYear() + "/" + ("0" + (date.getMonth() + 1)).slice(-2) + "/" + ("0" + date.getDay())).slice(-2);
+                $("#EndTime_Date").val(date.getFullYear() + "/" + ("0" + (date.getMonth() + 1)).slice(-2) + "/" + ("0" + date.getDate()).slice(-2));
                 $("#EndTime_Hour").val(date.getHours());
                 $("#EndTime_Minute").val(date.getMinutes());
                 //$("#Contents").val(json.Contents);
